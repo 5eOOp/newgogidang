d90584f0391ec0e22cf3df15292a7619a1f66a15
taehyun <kimth0526@gmail.com>
2021-04-14 21:37:10 +0900

    first commit

---
+++ src/main/java/me/seoop/newgogidang/NewgogidangApplication.java
@@ -0,0 +1,13 @@
+package me.seoop.newgogidang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgogidangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewgogidangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seoop/newgogidang/NewgogidangApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seoop.newgogidang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewgogidangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
